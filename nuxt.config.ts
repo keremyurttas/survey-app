c47e0c3c72e8a8745e4dca2a2e9cc02f3fb9f956
yusikerem <kerem.yky@gmail.com>
2023-07-11 09:36:52 +0300

    Implement firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    private: {},
+    public: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    },
+  },
   modules: ["@pinia/nuxt"],
 
   devtools: { enabled: true },
